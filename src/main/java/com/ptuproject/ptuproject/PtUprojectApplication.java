03c2530b9243ebffef3b7a5078c12bca7296a6a6
minhee baek <bsmq100@gmail.com>
2023-05-16 01:38:43 +0900

    first commit

---
+++ src/main/java/com/ptuproject/ptuproject/PtUprojectApplication.java
@@ -0,0 +1,13 @@
+package com.ptuproject.ptuproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PtUprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PtUprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ptuproject/ptuproject/PtUprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ptuproject.ptuproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PtUprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
